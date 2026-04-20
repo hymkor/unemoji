7f01f3cb641a55ff20a75ac012ff1f29a84e95dc
HAYAMA Kaoru <3752189+hymkor@users.noreply.github.com>
2026-04-20 09:55:22 +0900

    Support Miscellaneous Technical and Variation Selectors

--- main.go
+++ main.go
@@ -5,21 +5,29 @@ import (
 	"io"
 	"os"
 	"regexp"
-	"unicode/utf8"
+	"strings"
 )
 
-var rxEmoji = regexp.MustCompile(
-	"[\u2600-\u27BF\U0001F300-\U0001F9FF\U0001F1E6-\U0001F1FF]")
+var rxEmoji = regexp.MustCompile("[" +
+	"\u2300-\u23FF" + // Miscellaneous Technical
+	"\u2600-\u27BF" +
+	"\uFE00-\uFE0F" + // Variation Selectors
+	"\U0001F300-\U0001F9FF" +
+	"\U0001F1E6-\U0001F1FF" +
+	"]+")
+
+func unemojiString(s string) string {
+	var b strings.Builder
+	for _, c := range s {
+		fmt.Fprintf(&b, "&#x%X;", c)
+	}
+	return b.String()
+}
 
 func unemoji(r io.Reader, name string) error {
 	return eachLine(r, func(line string) error {
 		_, err := io.WriteString(os.Stdout,
-			rxEmoji.ReplaceAllStringFunc(
-				line,
-				func(s string) string {
-					u, _ := utf8.DecodeRuneInString(s)
-					return fmt.Sprintf("&#x%x;", u)
-				}))
+			rxEmoji.ReplaceAllStringFunc(line, unemojiString))
 		return err
 	})
 }
